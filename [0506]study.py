e112e270c7731dbe10749a70643e08baf95de3d9
HC-kang <weston0713@gmail.com>
2021-05-06 23:43:11 +0900

    0506 마무리

--- [0506]study.py
+++ [0506]study.py
@@ -42,9 +42,17 @@ def find_gcd(w,h):
 
 def find_gabage(w, h):
     gcd = find_gcd(w, h)
-    num = math.ceil(h/w) * w // gcd
-    return num * gcd
+    gabage = 0
+    for i in range(1, w//gcd+1):
+        gabage += math.ceil(i*h/w) - math.floor((i-1)*h/w)
+        #print(i,'회차',(i*h/w))
+        #print(i,'회차',math.ceil(i*h/w))
+        #print(i,'회차',math.floor((i-1)*h/w))
+        print(i,'회차',gabage)
+    gabage *= gcd
+    return gabage
 
+find_gabage(8,12)
 solution(12,8)
 
 # 원래 사각형에서 쓸수 있는 칸 result = w*h
@@ -59,3 +67,30 @@ solution(12,8)
 
 # 5/3
 
+#####
+# 똑똑한 로봇 청소기
+###
+
+N = 12  # 총 이동횟수
+
+def move(log):
+    # 맨 처음 위치 포함, 13개 지나가면 종료
+    if len(log) == N+1:
+        return 1
+
+    cnt = 0
+    # 전후좌우로 이동
+    for d in [[0,1], [0,-1], [1,0], [-1,0]]:
+        # 탐색이 안끝나면 계속 이동
+        next_pos = [log[-1][0] + d[0], log[-1][1] + d[1]]
+        # 로그에 다음 위치가 기록되어있는지 확인하기
+        check = False
+        for p in log:
+            if p[0] == next_pos[0] and p[1] ==next_pos[1]:
+                check = True # 있는 경우에 플래그를 True 로 변경
+        if check == False:
+            cnt += move(log + [next_pos])
+    return cnt
+
+print(move([[0, 0]]))
+    
