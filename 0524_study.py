6977bfedf7f0a0dbe0316a8174ee37d414502003
HC-kang <weston0713@gmail.com>
2021-05-24 17:02:46 +0900

    0524

--- 0524_study.py
+++ 0524_study.py
@@ -18,3 +18,23 @@ def quick_sort(a):
 d = [6, 8, 3, 9, 10, 1,2,4, 7, 5]
 quick_sort(d)
 print(d)
+
+
+#########################
+
+def bubble_sort(a):
+    n = len(a)
+    while True:
+        changed = False
+        for i in range(0, n-1):
+            if a[i] > a[i+1]:
+                print(a)  # 참고용
+                a[i], a[i+1] = a[i+1], a[i]
+                changed = True
+        if changed == False:
+            return
+
+d = [11,2,4,5,1,3,9,8,7,6,10]
+bubble_sort(d)
+print(d)
+
