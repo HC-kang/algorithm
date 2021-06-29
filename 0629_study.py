8e2900eff7cec77d17ca418f69d24510a4e431d7
HC-kang <weston0713@gmail.com>
2021-06-29 23:08:21 +0900

    0629

--- 0629_study.py
+++ 0629_study.py
@@ -35,3 +35,73 @@ count, bins, ignored = plt.hist(s, 30, normed
 =True)
 
 plt.plot(bins, 1/(sigma * np.sqrt(2*np.pi))*np.exp(-(bins-mu)**2/2*(sigma)))
+
+########################################################################################################
+
+# 세 정수를 입력받아 최댓값 구하기
+print('최댓값 구하기')
+a = int(input('a의 값'))
+b = int(input('b의 값'))
+c = int(input('c의 값'))
+
+maximum = a
+if b> maximum: maximum = b
+if c> maximum: maximum = c
+
+print(f'최댓값은 {maximum}')
+
+############################################################################################################
+print('이름을 입력하세요 : ', end = '')
+name= input()
+print(f'안녕하세요? {name}님')
+
+##############################################################################
+
+name = input('이름?:')
+print(f'안녕하세요? {name}님')
+
+##############################################################################
+
+int('17')
+
+int('110', 2)
+
+##############################################################################
+
+def max3(a,b,c):
+    maximum = a
+    if b>maximum: maximum=b
+    if c>maximum: maximum=c
+    return maximum
+
+print(f'max3(3,2,1) = {max3(3,2,1)}')
+print(f'max3(3,2,3) = {max3(3,2,3)}')
+print(f'max3(3,2,4) = {max3(3,2,4)}')
+print(f'max3(3,2,5) = {max3(3,2,5)}')
+print(f'max3(3,2,6) = {max3(3,2,6)}')
+print(f'max3(3,2,7) = {max3(3,2,7)}')
+print(f'max3(3,7,8) = {max3(3,7,8)}')
+print(f'max3(3,9,1) = {max3(3,9,1)}')
+print(f'max3(3,4,1) = {max3(3,4,1)}')
+
+##############################################################################
+
+def med3(a,b,c):
+    if a>=b:
+        if b>=c:
+            return b
+        elif c>=a:
+            return a
+    elif a>c:
+        return a
+    elif b>c:
+        return c
+    else:
+        return b
+
+print('세 정수의 중앙값')
+a = int(input('a'))
+b = int(input('b'))
+c = int(input('c'))
+
+print(f'중앙값 {med3(a,b,c)}')
