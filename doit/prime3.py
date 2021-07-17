dad9a669ab95a33f6c67e729233514a8b1a632cd
HC-kang <weston0713@gmail.com>
2021-07-17 13:45:47 +0900

    0717

---
+++ doit/prime3.py
@@ -0,0 +1,25 @@
+counter = 0
+ptr = 0
+prime = [None] * 500
+
+prime[ptr] = 2
+ptr += 1
+
+prime[ptr] = 3
+ptr += 1
+
+for n in range(5, 1001, 2):
+    i = 1
+    while prime[i] * prime[i] <=n:
+        counter += 2
+        if n % prime[i] == 0:
+            break
+        i += 1
+    else:
+        prime[ptr] = n
+        ptr += 1
+        counter += 1
+
+for i in range(ptr):
+    print(prime[i])
+print(f'곱셈과 나눗셈을 실행한 횟수:{counter}')
