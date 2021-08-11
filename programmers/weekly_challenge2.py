9caa035e92a8b2e7206cc72fcde195a870ab5fe6
HC-kang <weston0713@gmail.com>
2021-08-11 09:42:20 +0900

    0811

--- programmers/weekly_challenge2.py
+++ programmers/weekly_challenge2.py
@@ -1,5 +1,7 @@
 scores = [[100,90,98,88,65],[50,45,99,85,77],[47,88,95,80,67],[61,57,100,80,65],[24,90,94,75,65]]
-
+scores_t = [[i[j] for i in scores] for j in range(len(scores))]
+scores_t = [[i[j] for i in scores] for j in range(len(scores))]
+scores_t = [[0 for i in range(len(scores))] for i in range(len(scores))]
 
 
 
